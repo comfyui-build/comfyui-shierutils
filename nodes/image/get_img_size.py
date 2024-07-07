f779b33ed7cd16fc2c5c21d82cfcb6531eb8e5c7
shiertier <junjie.text@gmail.com>
2024-07-07 21:21:02 +0800

    update

--- nodes/image/get_img_size.py
+++ nodes/image/get_img_size.py
@@ -24,4 +24,30 @@ output:
     INT: 图像的高度。
 '''
 
-from ...thirdparty.stability_ComfyUI_nodes.get_img_size import *
+
+class GetImageSize:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+            }
+        }
+
+    RETURN_TYPES = ("INT", "INT")
+    RETURN_NAMES = ("width", "height")
+
+    FUNCTION = "get_size"
+
+    CATEGORY = "stability/image"
+
+    def get_size(self, image):
+        _, height, width, _ = image.shape
+        return (width, height)
+
+NODE_CLASS_MAPPINGS = {
+    "GetImageSize": GetImageSize
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+}
